fde2e09cae1400daa63bafeab2ccd9caee8c4ddb
HelloKittycoder <shucheng2015@outlook.com>
2021-10-06 12:26:34 +0800

    62-65 分析脚手架结构、render函数、默认配置、ref属性

---
+++ VueStudy/vue_test/src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+      <h1 ref="title" v-text="msg"></h1>
+      <button ref="btn" @click="showDOM">点我输出上方的DOM</button>
+      <School id="sch" ref="sch"/>
+  </div>
+</template>
+
+<script>
+    // 引入School组件
+    import School from './components/School.vue'
+
+    export default {
+        name:'App',
+        components:{School},
+        data(){
+            return {
+                msg:'欢迎学习Vue！'
+            }
+        },
+        methods: {
+            showDOM() {
+                console.log(this.$refs.title) // 真实DOM元素
+                console.log(this.$refs.btn) // 真实DOM元素
+                console.log(this.$refs.sch) // School组件的实例对象（vc）
+            }
+        },
+    }
+</script>
+
+<style>
+
+</style>
