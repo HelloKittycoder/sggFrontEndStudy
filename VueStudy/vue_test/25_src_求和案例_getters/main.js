825a0361f9bead004db072724f52e4b3c6ba56db
HelloKittycoder <shucheng2015@outlook.com>
2021-10-16 17:22:11 +0800

    111 getters配置项

---
+++ VueStudy/vue_test/25_src_求和案例_getters/main.js
@@ -0,0 +1,21 @@
+// 引入Vue
+import Vue from 'vue'
+// 引入App
+import App from './App.vue'
+// 引入store
+import store from './store'
+
+// 关闭Vue的生产提示
+Vue.config.productionTip = false
+
+// 创建vm
+const vm = new Vue({
+    el:'#app',
+    render:h=>h(App),
+    store,
+    beforeCreate() {
+        Vue.prototype.$bus = this
+    }
+})
+
+// console.log(vm)
